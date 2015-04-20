be3e0af5f91511e43779bcf2129ef5596f5d4503
Alok Rajiv <mail@alokrajiv.com>
2015-04-20 18:50:39 +0400

    Switched to prepared statments for security

--- handles/login_handle.php
+++ handles/login_handle.php
@@ -8,17 +8,17 @@
 require '../configs/db_config.php';
 $usernameFromPost = $_POST['login-id'];
 $passwordFromPost = $_POST['login-password'];
-$sql = "SELECT `user-id`, `login-password`, `user-type` FROM `login_creds` where `user-id` like '$usernameFromPost'";
-$res="";
-foreach ($conn->query($sql) as $row) {
-    $res = $row;
-}
+$sql = "SELECT `user-id`, `login-password`, `user-type` FROM `login_creds` where `user-id` = ? LIMIT 1";
+$sth = $conn->prepare($sql);
+$sth->execute(array( $usernameFromPost));
+$res = $sth->fetch();
 $hashedPasswordFromDB = $res["login-password"];
 if (password_verify($passwordFromPost, $hashedPasswordFromDB)) {
     echo 'Password is valid!';
     session_start();
     $_SESSION['user-id']= $res["user-id"];
     $_SESSION['user-type']= $res["user-type"];
+    header("Location: ../home");
 } else {
     echo 'Invalid password.';
 }
